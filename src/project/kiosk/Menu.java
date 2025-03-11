a4604a33d341f7d614ddbeca78749202d1c87c31
김민준 <aaaz7531@gmail.com>
2025-03-11 19:44:49 +0900

    키오스ê·크과제2

--- src/project/kiosk/Menu.java
+++ src/project/kiosk/Menu.java
@@ -1,4 +1,51 @@
 package project.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Menu {
+    List<MenuItem> menuItem = new ArrayList<MenuItem>();
+    Scanner sc = new Scanner(System.in);
+
+
+    public Menu() {
+        // 리스트 추가
+        menuItem.add(new MenuItem("Burger",0,"버거 모음"));
+        menuItem.add(new MenuItem("Drinks",0,"음료 모음"));
+        menuItem.add(new MenuItem("Desserts",0,"디저트 모음"));
+
+        while (true) { // 반복문 시작
+
+            System.out.println("[ " + "MAIN MENU" + " ]");
+            for (int i = 0; i < menuItem.size(); i++) { // 리스트에있는 메뉴 출력
+                System.out.println(i + 1 + ". " + menuItem.get(i).getName() + " | W " + menuItem.get(i).getPrice() + " | " + menuItem.get(i).getEx());
+            }
+            System.out.println("0." + " 종료   " + " | " + " 종료 ");
+
+            System.out.println("메뉴를 선택하세요.");
+            int order = sc.nextInt(); // 입력 받기
+
+            switch (order) {
+                case 1:
+                    new Kiosk().start(); // 1 입력받을시 BURGERS MENU로 이동
+                    return;
+                case 2:
+                    System.out.println("Drinks 메뉴를 선택했습니다.");
+                    return;
+                case 3:
+                    System.out.println("Desserts 메뉴를 선택했습니다.");
+                    return;
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                    break;
+            }
+
+
+        }
+
+    }
 }
--- src/project/kiosk/MenuItem.java
+++ src/project/kiosk/MenuItem.java
@@ -1,27 +1,39 @@
 package project.kiosk;
 
-public class MenuItem {
+public class MenuItem { // 속성
     private String name;
     private double price;
     private String ex;
 
-    public MenuItem(String name, double price, String ex) {
+    public MenuItem(String name, double price, String ex) { // 생성자
         this.name = name;
         this.price = price;
         this.ex = ex;
     }
 
-    public String getname() {
+    public String getName() { // name 게터
         return name;
     }
 
-    public double getprice() {
+    public double getPrice() { // price(가격) 게터
         return price;
     }
 
-    public String getex() {
+    public String getEx() { // ex(설명) 게터
         return ex;
     }
 
+    public void setName(String name) { // name 세터
+        this.name = name;
+    }
+    public void setPrice(double price) { // price(가격) 세터
+        if (price < 0) {
+            System.out.println("가격은 음수가 될 수 없습니다."); // 가격이 0보다 아래일경우
+        }
+        this.price = price;
+    }
+    public void setEx(String ex) { // ex(설명) 세터
+        this.ex = ex;
+    }
 
 }
